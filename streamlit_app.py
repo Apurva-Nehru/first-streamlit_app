9937ca342b102023c9f22132e023376d7d1fd997
Apurva-Nehru <91097960+Apurva-Nehru@users.noreply.github.com>
2022-06-04 17:17:18 -0400

    header

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,9 +2,11 @@ import streamlit
 
 streamlit.title( 'My Parents New Healthy Diner')
 
-streamlit.header ('Build Your Own Fruit Smoothie' )
 
 streamlit.header ('Breakfast Menu')
 streamlit.text ( 'Omega 3 & Blueberry 0atmeal ')
 streamlit. text ( 'Kale, Spinach& Rocket Smoothie')
 streamlit . text ( 'Hard -Boiled Free- Range Egg')
+
+
+streamlit.header ('Build Your Own Fruit Smoothie' )
